02105b50421eae4d0d0dce87835174ea97454002
Kubilay Akdemir <kubidik27@gmail.com>
2023-02-01 15:51:08 +0300

    WeatherData model created

--- WeatherApp/Model/WeatherData.swift
+++ WeatherApp/Model/WeatherData.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let weather: [Weather]
+    let main: Main
+    let wind: Wind
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feels_like: Double
+    let pressure: Int
+    let humidity: Int
+}
+
+struct Wind: Codable {
+    let speed: Double
+}
