ea9af713f8e0b7ef72940854e84355a45d135a22
Kubilay Akdemir <kubidik27@gmail.com>
2023-02-02 15:47:45 +0300

    current location cell created

--- WeatherApp/Controller/CurrentLocationTableViewCell.swift
+++ WeatherApp/Controller/CurrentLocationTableViewCell.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CurrentLocationTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var minMaxLabel: UILabel!
+    
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        weatherManager.delegate = self
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +34,38 @@ class CurrentLocationTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+}
+
+extension CurrentLocationTableViewCell: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitute: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+}
+
+extension CurrentLocationTableViewCell: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.tempLabel.text = weather.temperatureString
+            self.descriptionLabel.text = weather.description
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
     
 }
