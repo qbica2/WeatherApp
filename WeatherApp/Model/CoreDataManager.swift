2103c3ac618827cb57bd412a75e54bf0370071f2
Kubilay Akdemir <kubidik27@gmail.com>
2023-02-03 20:23:49 +0300

    core data manager created

---
+++ WeatherApp/Model/CoreDataManager.swift
@@ -0,0 +1,59 @@
+//
+//  CoreDataManager.swift
+//  WeatherApp
+//
+//  Created by Mehmet Kubilay Akdemir on 3.02.2023.
+//
+
+import CoreData
+import UIKit
+
+
+struct CoreDataManager {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var idArray = [UUID]()
+    var cityArray = [String]()
+    
+    mutating func fetchData(entityName: String) -> Bool {
+        
+        idArray.removeAll(keepingCapacity: false)
+        cityArray.removeAll(keepingCapacity: false)
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        
+        
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                if let cityName = data.value(forKey: "cityName") as? String {
+                    cityArray.append(cityName)
+                }
+                if let id = data.value(forKey: "id") as? UUID {
+                    idArray.append(id)
+                }
+
+            }
+            return true
+        } catch {
+            print("error")
+            return false
+        }
+    }
+    
+    func saveData (cityName: String, id: UUID, entityName: String) {
+        let object = NSEntityDescription.insertNewObject(forEntityName: entityName , into: context)
+        object.setValue(cityName, forKey: "cityName")
+        object.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+    
+}
